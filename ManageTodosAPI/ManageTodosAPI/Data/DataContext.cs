8379e7cd5869c291e392c2670de07d846304596e
Mohan Rathod <mohanrathod31@mail.com>
2024-04-26 15:35:55 +0530

    EF core and Db Connection done

---
+++ ManageTodosAPI/ManageTodosAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using ManageTodosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManageTodosAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ ManageTodosAPI/ManageTodosAPI/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿namespace ManageTodosAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        public string Task { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime? Deadline { get; set; }
+
+    }
+}
